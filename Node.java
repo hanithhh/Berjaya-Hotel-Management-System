c7978adc67fa5c62435bf111b1b84e67a8d8a0b2
nurainaa <139862495+NanaBalqis@users.noreply.github.com>
2023-11-26 00:30:52 +0800

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,17 @@
+# Berjaya-Hotel-Management-System
+
+  public class Node{
+   Object data;
+   Node next;
+   //default constructor
+   public Node(){
+      data=null;
+      next=null;
+   }
+   //normal contructor
+   public Node(Object data){
+      this.data=data;
+      next=null;
+   }
+ }
+ 
